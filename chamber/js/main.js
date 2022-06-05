036d8eb1b48436d3834b70fefaccf4a3dcde2082
Edgar Valderrama <val22001@byui.edu>
2022-06-04 21:25:11 -0500

    fixes

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -19,52 +19,54 @@ if (day == 1 || day == 2) {
 }
 
 
-//Lazy loading
-const imagesToLoad = document.querySelectorAll("img[data-src]");
+if (document.querySelector('.content.discover')) {
+    //Lazy loading
+    const imagesToLoad = document.querySelectorAll("img[data-src]");
 
-const imgOptions = {
-    threshold: 0,
-    rootMargin: "0px 0px 50px 0px"
-};
+    const imgOptions = {
+        threshold: 0,
+        rootMargin: "0px 0px 50px 0px"
+    };
 
-const loadImages = (image) => {
-    image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload = () => { image.removeAttribute('data-src'); };
-};
+    const loadImages = (image) => {
+        image.setAttribute('src', image.getAttribute('data-src'));
+        image.onload = () => { image.removeAttribute('data-src'); };
+    };
 
-if ('IntersectionObserver' in window) {
-    const imgObserver = new IntersectionObserver(items => {
-        items.forEach((item) => {
-            if (item.isIntersecting) {
-                loadImages(item.target);
-                imgObserver.unobserve(item.target);
-            }
-        });
-    }, imgOptions);
+    if ('IntersectionObserver' in window) {
+        const imgObserver = new IntersectionObserver(items => {
+            items.forEach((item) => {
+                if (item.isIntersecting) {
+                    loadImages(item.target);
+                    imgObserver.unobserve(item.target);
+                }
+            });
+        }, imgOptions);
 
-    imagesToLoad.forEach(img => imgObserver.observe(img));
-} else {
-    imagesToLoad.forEach(img => {loadImage(img)});
-}
+        imagesToLoad.forEach(img => imgObserver.observe(img));
+    } else {
+        imagesToLoad.forEach(img => {loadImage(img)});
+    }
 
-//Local storage
-const userVisit = document.querySelector(".last-visit");
+    //Local storage
+    const userVisit = document.querySelector(".last-visit");
 
-// get the stored value in localStorage
-const lastVisit = localStorage.getItem("last_visit");
+    // get the stored value in localStorage
+    const lastVisit = localStorage.getItem("last_visit");
 
-const currentTime = new Date().getTime();
-const lastVisitInDays = lastVisit ? numberOfDays(currentTime, lastVisit) : 0;
+    const currentTime = new Date().getTime();
+    const lastVisitInDays = lastVisit ? numberOfDays(currentTime, lastVisit) : 0;
 
-if (lastVisitInDays > 0) {
-  time = lastVisitInDays > 1 ? 'days' : 'day';
-	userVisit.textContent = `${lastVisitInDays} ${time} ago`;
-} else {
-	userVisit.textContent = 'Today';
-}
+    if (lastVisitInDays > 0) {
+    time = lastVisitInDays > 1 ? 'days' : 'day';
+        userVisit.textContent = `${lastVisitInDays} ${time} ago`;
+    } else {
+        userVisit.textContent = 'Today';
+    }
 
-localStorage.setItem("last_visit", currentTime);
+    localStorage.setItem("last_visit", currentTime);
 
-function numberOfDays(date1, date2){
-    return parseInt((date1 - date2) / 1000 / 3600 / 24);
+    function numberOfDays(date1, date2){
+        return parseInt((date1 - date2) / 1000 / 3600 / 24);
+    }
 }
